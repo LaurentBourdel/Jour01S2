350a13d57ae5c2786bf56e2f5d19e32be3c03617
LaurentBourdel <62954361+LaurentBourdel@users.noreply.github.com>
2020-04-13 18:23:18 -0400

    Add files via upload

--- tests-ruby-master/lib/01_temperature.rb
+++ tests-ruby-master/lib/01_temperature.rb
@@ -1,3 +1,15 @@
+def ask_conv
+  puts " "
+  puts "> Conversion: (C -> F) tape c - (F -> C) tape f"
+  print "> "
+  return gets.chomp.to_s
+end
+
+def ask_temp
+  puts "> Temperature?"
+  print "> "
+  return gets.chomp.to_f
+end
 
 def ftoc(temp_f)
 	temp_c = (temp_f - 32) * 5 / 9
@@ -9,3 +21,19 @@ def ctof(temp_c)
 	return temp_f
 end
 
+def temp_converter
+	conv = ask_conv
+	if conv == "f"
+  		t_c = ftoc(ask_temp)
+  		puts "> temperature (C) = #{t_c}"
+  		puts " "
+  	end
+  	if conv == "c"
+  		t_f = ctof(ask_temp)
+  		puts "> temperature (F) = #{t_f}"
+  		puts " "
+  	end
+	
+end
+
+temp_converter
